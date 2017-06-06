e8b4fa9321aeae17e4c71aae8408135ae475f165
Cameron Jenner <cjenner37@gmail.com>
2017-06-06 14:35:25 -0600

    all done

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ function Multiplier(x) {
 	this.x = x;
 	this.currentValue = 1;
 	this.multiply = function(){
-		this.currentValue = currentValue * x;
-		return this.currentValue;
+		this.currentValue = this.currentValue * x;
 	}
 	this.getCurrentValue = function () {
-		return currentValue;
+		console.log(this.currentValue);
 	};
 };
 
+console.log("Problem #1:");
+console.log("initialize multiplier function to multiply by 4... var myMultiplier = new Multiplier(4)")
 var myMultiplier = new Multiplier(4);
+console.log("get current value: myMultiplier.getCurrentValue()");
+myMultiplier.getCurrentValue();
+console.log("multiply (4)... myMultiplier.multiply()");
+myMultiplier.multiply();
+console.log("get current value: myMultiplier.getCurrentValue()");
+myMultiplier.getCurrentValue();
+console.log("multiply again... myMultiplier.multiply()");
+myMultiplier.multiply();
+console.log("multiply again... myMultiplier.multiply()");
+myMultiplier.multiply();
+console.log("get current value: myMultiplier.getCurrentValue()");
+myMultiplier.getCurrentValue();
+
 
 
 /*
@@ -38,7 +52,7 @@ function Album() {
 		};
 	};
 	this.findPhoto = function(photo_location) {
-		return this.photos[photo_location - 1];
+		console.log(this.photos[photo_location - 1]);
 	}
 }
 
@@ -47,13 +61,21 @@ function Photo(name, location) {
 	this.location = location;
 }
 
+console.log("Problem #2:");
+console.log("make three photos and one album...");
 var firstPhoto = new Photo("1.jpg", "uptown");
 var secondPhoto = new Photo("2.jpg", "downtown");
 var thirdPhoto = new Photo("3.jpg", "midtown");
 var myAlbum = new Album();
+console.log("add photos to album... myAlbum.addPhoto(firstPhoto) (for three photos)");
 myAlbum.addPhoto(firstPhoto);
 myAlbum.addPhoto(secondPhoto);
 myAlbum.addPhoto(thirdPhoto);
+console.log("list all photos: myAlbum.listAllPhotos()");
+myAlbum.listAllPhotos();
+console.log("find the second photo: myAlbum.findPhoto(2)");
+myAlbum.findPhoto(2);
+
 
 /*
  * 3.
@@ -77,38 +99,43 @@ myAlbum.addPhoto(thirdPhoto);
  		let book = this.books.filter(function (el) {
  			return (el.title === book_title);
  		});
- 		return book[0];
+ 		console.log(book[0]);
  	}
-
-
  	this.getBookIndex = function (book_title) {
  		indexOfBook = this.books.indexOf(this.getBook(book_title));
  	}
-
-
  	this.addBook = function (book) {
  		this.books.push(book);
  	}
-
-
  	this.removeBook = function (book_title) {
  		this.getBookIndex(book_title);
  		this.books.splice(indexOfBook, 1);
  	}
-
-
  	this.countBooks = function () {
- 		return this.books.length;
+ 		console.log(this.books.length);
  	}
  }
 
+console.log("Problem #3");
+console.log("make three books and a library...");
  var firstBook = new Book ("Harry Potter", "J K Rowling", "Fantasy");
  var secondBook = new Book ("Stormlight Archive", "Brandon Sanderson", "Fantasy");
  var thirdBook = new Book ("The Fractal Geometry of Nature", "Benoit Mandlebrot", "Nonfiction");
  var myLibrary = new Library;
+console.log("count books in library:  myLibrary.countBooks()");
+ myLibrary.countBooks();
+console.log("add books to library...  myLibrary.addBook(firstBook) (three times)");
  myLibrary.addBook(firstBook);
  myLibrary.addBook(secondBook);
  myLibrary.addBook(thirdBook);
+console.log("count books:  myLibrary.countBooks()");
+ myLibrary.countBooks();
+console.log("get Stormlight Archive:  myLibrary.getBook(\"Stormlight Archive\")");
+ myLibrary.getBook("Stormlight Archive");
+console.log("remove Harry Potter...  myLibrary.removeBook(\"Harry Potter\")");
+ myLibrary.removeBook("Harry Potter");
+console.log("count books:  myLibrary.countBooks()");
+ myLibrary.countBooks();
 
 
 
@@ -121,19 +148,71 @@ myAlbum.addPhoto(thirdPhoto);
   * object that should be able to store instances of students and teachers. Make sure to write code afterwards 
   * that creates instances of these objects to make sure that what you've written works well and you're able to 
   * store the necessary data in each object.
+  *
+  * 2.
+  * Give the school the ability to hire new teachers, and take on more students. This will be done with one 
+  * method of school, not two. This method will add teachers to the list of teachers, and students to the 
+  * list of students.
   */
 
-function Person () {
-
+function Person (name, gender, age, height) {
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.height = height;
 };
 
-function Teacher(argument) {
-	// body...
+function Teacher(name, gender, age, height, years_taught, subject, nice) {
+	Person.call(this, name, gender, age, height);
+
+	this.years_taught = years_taught;
+	this.subject = subject;
+	this.nice = nice;
 }
 
-  /*
-   * 2.
-   * Give the school the ability to hire new teachers, and take on more students. This will be done with one 
-   * method of school, not two. This method will add teachers to the list of teachers, and students to the 
-   * list of students.
-   */
+function Student(name, gender, age, height, grade, well_behaved) {
+	Person.call(this, name, gender, age, height);
+
+	this.grade = grade;
+	this.well_behaved = well_behaved;
+}
+
+function School() {
+	this.people = [];
+	this.teachers = [];
+	this.students = [];
+
+	this.addPerson = function (person) {
+		if (person.constructor == Teacher){
+			this.people.push(person);
+			this.teachers.push(person);
+		} else {
+			this.people.push(person);
+			this.students.push(person);
+		}
+	}
+	this.viewPerson = function (personName) {
+		let person = this.people.filter(function (el) {
+ 			return (el.name === personName);
+ 		});
+ 		console.log(person[0]);
+	}
+}
+
+console.log("Advanced Problem #1:");
+console.log("make two students and two teachers and one school...");
+var billy = new Student("Billy", "male", 11, "4\'8\"", 4, false);
+var emily = new Student("Emily", "female", 14, "5\'5\"", 8, true);
+var ron = new Teacher("Ronald", "male", 45, "6\'1\"", 20, "physics", false);
+var susan = new Teacher("Susan", "female", 33, "5\'4\"", 10, "art", true);
+var mySchool = new School;
+console.log("add people to school:  mySchool.addPerson(billy) (for four people)");
+mySchool.addPerson(billy);
+mySchool.addPerson(emily);
+mySchool.addPerson(ron);
+mySchool.addPerson(susan);
+console.log("view someone in the school: mySchool.viewPerson(\"Susan\")");
+mySchool.viewPerson("Susan");
+console.log("view someone else: mySchool.viewPerson(\"Billy\")");
+mySchool.viewPerson("Billy");
+
